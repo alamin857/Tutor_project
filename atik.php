a9e0e0a8b8de15d1ac475b9a427bba315127614d
alamin857 <63539926+alamin857@users.noreply.github.com>
2020-04-12 17:15:06 +0600

    Add files via upload

---
+++ atik.php
@@ -0,0 +1,24 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div class="box">
+	<img src="usr.png" class="usrimg">
+	<h2>Login to your account</h2>
+	<form method="POST" action="">
+		<p>Email</p>
+		<input type="email" name="email" placeholder="Enter Email" required>
+		<p>Password</p>
+		<input type="Password" name="password" placeholder="Enter Password" required>
+		<input type="submit" value="Login"><br>
+		<a href="#">Forget Password ?</a>
+		
+	</form>
+</div>
+</body>
+</html>
